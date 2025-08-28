2128debed301dd553b6814b8be082b133c9f3512
Jonathan Pittman <jonathanrppittman@gmail.com>
2025-08-28 13:34:15 -0400

    added slurm script that starts training

--- src/utils/MakeTorchGraphData.sh
+++ src/utils/MakeTorchGraphData.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
-#SBATCH --job-name MakeTorchGraphData
-#SBATCH --output=/isilon/datalake/lcbn_research/final/beach/JonathanP/MakeTorchGraphDataOutput.txt
-#SBATCH --error=/isilon/datalake/lcbn_research/final/beach/JonathanP/MakeTorchGraphDataError.txt
+#SBATCH --job-name GUINCandaNeuroGraphTest
+#SBATCH --output /isilon/datalake/lcbn_research/final/beach/JonathanP/GUINCandaNeuroGraphTest.txt
+#SBATCH --error /isilon/datalake/lcbn_research/final/beach/JonathanP/GUINCandaNeuroGraphTest.txt
 #SBATCH --nodes 1
 #SBATCH -p gpu-h100
 #SBATCH --gpus 2
 ##SBATCH --mem 1850000M
 #SBATCH --mem 700000M
-#SBATCH --time 24:00:00
+#SBATCH --time 4:00:00
 
 module purge
 module load cuda-toolkit/11.8.0
@@ -16,4 +16,4 @@ conda activate NeuroGraph
 
 cd /isilon/datalake/lcbn_research/final/beach/JonathanP
 ##srun python MakeTorchGraphData.py
-srun python NCandaToTorchGraphDataGUITest.py --threshold .1 --net_number 3
+srun /isilon/datalake/lcbn_research/final/beach/JonathanP/lcbn_gnn_gui/src/utils/NCandaNeuroGraphTest.sh --data "/isilon/datalake/lcbn_research/final/beach/JonathanP/lcbn_gnn_gui/NCandaData500_cddr15a_5pct.pt" --model GCN
---
+++ src/utils/NCandaNeuroGraphTest.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+
+#SBATCH --job-name NCanda_500_50pct
+#SBATCH --output=/isilon/datalake/lcbn_research/final/beach/JonathanP/NeuroGraph/outputs/NCanda500_10pct_TripleNets3_GATNeuroGraphTestOutput.txt
+#SBATCH --error=/isilon/datalake/lcbn_research/final/beach/JonathanP/NeuroGraph/errors/NCanda500_10pct_TripleNets3_GATNeuroGraphTestError.txt
+#SBATCH --nodes 1
+#SBATCH -p gpu
+#SBATCH --gpus 4
+##SBATCH --mem 1850000M
+#SBATCH --mem 480000M
+#SBATCH --time 24:00:00
+
+module purge
+module load cuda-toolkit/11.8.0
+
+conda activate NeuroGraph
+
+cd /isilon/datalake/lcbn_research/final/beach/JonathanP/NeuroGraph
+
+srun python main_NCanda.py \
+ --model 'GATConv' \
+ --device 'cuda' \
+ --batch_size 16 \
+ --hidden 64 \
+ --seed 123 \
+ --threshold 0.1 \
+ --num_nodes 500 \
+ --epochs 500 \
+ --early_stopping 10 \
+ --trip_net_num '3'
