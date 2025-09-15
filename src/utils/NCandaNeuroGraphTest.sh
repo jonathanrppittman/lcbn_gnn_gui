9dbb4d6d269ef065612967a7801b84650c44af19
Jonathan Pittman <jonathanrppittman@gmail.com>
2025-09-15 15:49:31 -0400

    changed PyQt5 default version for quick fix

--- src/utils/NCandaNeuroGraphTest.sh
+++ src/utils/NCandaNeuroGraphTest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 
-#SBATCH --job-name GUINCandaNeuroGraphTest
-#SBATCH --output=/isilon/datalake/lcbn_research/final/beach/JonathanP/NeuroGraph/outputs/NCanda500_10pct_TripleNets3_GATNeuroGraphTestOutput.txt
-#SBATCH --error=/isilon/datalake/lcbn_research/final/beach/JonathanP/NeuroGraph/errors/NCanda500_10pct_TripleNets3_GATNeuroGraphTestError.txt
+#SBATCH --job-name GNN_NCANDA_GUI
+#SBATCH --output /isilon/datalake/lcbn_research/final/beach/JonathanP/GUI_NCANDA_GNN_TEST_OUT.txt
+#SBATCH --error /isilon/datalake/lcbn_research/final/beach/JonathanP/GUI_NCANDA_GNN_TEST_ERROR.txt
 #SBATCH --nodes 1
 #SBATCH -p gpu-h100
 #SBATCH --gpus 2
@@ -16,16 +16,15 @@ module load cuda-toolkit/11.8.0
 conda activate NeuroGraph
 
 cd /isilon/datalake/lcbn_research/final/beach/JonathanP/NeuroGraph
-
 srun python main_NCanda.py \
- --data "/isilon/datalake/lcbn_research/final/beach/JonathanP/NCandaData500_cddr15a_5pct.pt" \
- --model GCNConv \
- --device 'cuda' \
- --batch_size 16 \
- --hidden 64 \
- --seed 123 \
- --threshold 0.1 \
- --num_nodes 500 \
- --epochs 500 \
- --early_stopping 10 \
- --trip_net_num '3'
+  --model 'GATConv' \
+  --data 'NCandaData500cddr15a_10pct.pt' \
+  --device 'cuda' \
+  --batch_size 16 \
+  --hidden 64 \
+  --seed 123 \
+  --threshold 0.1 \
+  --num_nodes 500 \
+  --epochs 500 \
+  --early_stopping 10 \
+  --trip_net_num '3'
